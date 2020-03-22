2af5b6a7e0017376c900aad99936e738955f48d3
Jesse <jessejose@outlook.es>
2020-03-22 13:56:28 -0400

    Domain models created

---
+++ Source/TimeTracker.Domain/Schedule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TimeTracker.Domain
+{
+    /// <summary>
+    /// Represents a day of work.
+    /// </summary>
+    public class Schedule : ISchedule
+    {
+        /// <inheritdoc/>
+        public int Id { get; set; }
+        /// <inheritdoc/>
+        public DateTimeOffset ScheduleDate { get; set; }
+        /// <inheritdoc/>
+        public ICollection<Task> Tasks { get; } = new List<Task>();
+    }
+}
--- Source/TimeTracker.Uwp/MainPage.xaml.cs
+++ Source/TimeTracker.Uwp/MainPage.xaml.cs
@@ -1,19 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+﻿using Windows.UI.Xaml.Controls;
 
 namespace TimeTracker.Uwp
 {
@@ -22,6 +7,9 @@ namespace TimeTracker.Uwp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public MainPage()
         {
             this.InitializeComponent();
