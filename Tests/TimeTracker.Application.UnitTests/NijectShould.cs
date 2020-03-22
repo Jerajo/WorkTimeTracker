3142d61ee213a0da919e48c5069034a2bd0d59b3
Jesse <jessejose@outlook.es>
2020-03-22 18:08:46 -0400

    Ninject injection are working

---
+++ Tests/TimeTracker.Application.UnitTests/NijectShould.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
+using TimeTracker.Application.Query;
+
+namespace TimeTracker.Application.UnitTests
+{
+    [TestClass]
+    public class NijectShould
+    {
+        private IKernel _sut;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _sut = new StandardKernel(new TestModule());
+        }
+
+        [TestMethod]
+        public void GetGroupsQuery()
+        {
+            var result = _sut.Get<GetGroupsQuery>();
+
+            Assert.IsInstanceOfType(result, typeof(GetGroupsQuery));
+        }
+
+        [TestMethod]
+        public void GetTasksQuery()
+        {
+            var result = _sut.Get<GetTasksQuery>();
+
+            Assert.IsInstanceOfType(result, typeof(GetTasksQuery));
+        }
+    }
+}
---
+++ Tests/TimeTracker.Application.UnitTests/TestModule.cs
@@ -0,0 +1,16 @@
+﻿using Ninject.Modules;
+using System.Collections.Generic;
+using TimeTracker.Application.Contracts;
+using TimeTracker.Application.Query;
+
+namespace TimeTracker.Application.UnitTests
+{
+    internal class TestModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IQuery<Domain.Group, List<Domain.Group>>>().To<GetGroupsQuery>();
+            Bind<IQuery<Domain.Task, List<Domain.Task>>>().To<GetTasksQuery>();
+        }
+    }
+}
