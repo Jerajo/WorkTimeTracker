14a31c4b9e93fceed8a374d06c37c5421b76a32a
Jesse <jessejose@outlook.es>
2020-04-04 09:26:17 -0400

    getTaskQuery and taskValidator tested

--- Tests/TimeTracker.Application.UnitTests/Queries/GetTasksQueryShould.cs
+++ Tests/TimeTracker.Application.UnitTests/Queries/GetTasksQueryShould.cs
@@ -2,7 +2,9 @@
 using Ninject;
 using System;
 using System.Collections.Generic;
-using TimeTracker.Application.Factories;
+using System.Linq;
+using TimeTracker.Application.Commands;
+using TimeTracker.Application.Dtos;
 using TimeTracker.Application.Queries;
 using TimeTracker.Domain;
 using AsyncOperation = System.Threading.Tasks.Task;
@@ -12,30 +14,46 @@ namespace TimeTracker.Application.UnitTests.Queries
     [TestClass]
     public class GetTasksQueryShould
     {
-        private GetTasksQuery _Sut;
+        private IKernel _kernel;
+        private GetTasksQuery _sut;
 
         [TestInitialize]
         public void TextInitialize()
         {
-            var factory = AssemblyConfiguration.Kernel.Get<QueryFactory>();
-
-            _Sut = factory.GetInstance<GetTasksQuery>();
+            _kernel = AssemblyConfiguration.Kernel;
+            _sut = _kernel.Get<GetTasksQuery>();
         }
 
         [TestMethod]
         public void GuardAgainstNullArguments()
         {
-            Assert.ThrowsException<ArgumentNullException>(() => _Sut.Run(null));
+            Assert.ThrowsException<ArgumentNullException>(() => _sut.Run(null));
         }
 
         [TestMethod]
-        public async AsyncOperation ReturnEmptyListOrAListOfTasks()
+        public async AsyncOperation ReturnAEmptyListOfTasks()
         {
-            var emptyList = new List<Task>();
-            var result = await _Sut.Run(new Task());
+            var result = await _sut.Run(x => x.Id < 0);
 
-            Assert.IsInstanceOfType((object)result, typeof(List<Task>));
-            CollectionAssert.AreEqual(result, emptyList);
+            Assert.IsInstanceOfType(result, typeof(List<Task>));
+            Assert.IsFalse(result.Any());
+        }
+
+        [TestMethod]
+        public async AsyncOperation ReturnAListOfTasks()
+        {
+            var createTask = _kernel.Get<CreateTaskCommand>();
+            await createTask.Run(new TaskDto
+            {
+                Code = "4526",
+                Name = "Hola",
+                DescriptionId = 1
+            });
+
+            var result = await _sut.Run(x => true);
+
+            Assert.IsInstanceOfType(result, typeof(List<Task>));
+            Assert.IsTrue(result.Any());
         }
     }
 }
