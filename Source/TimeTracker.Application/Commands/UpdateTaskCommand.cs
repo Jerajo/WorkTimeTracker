7fb446c465e019bd39eaeba41fde14c40bf2038a
Jesse <jessejose@outlook.es>
2020-04-04 18:21:55 -0400

    Update task command and tests created

--- Source/TimeTracker.Application/Commands/UpdateTaskCommand.cs
+++ Source/TimeTracker.Application/Commands/UpdateTaskCommand.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Ardalis.GuardClauses;
+using AutoMapper;
+using FluentValidation;
+using System;
 using TimeTracker.Application.Contracts;
+using TimeTracker.Application.Dtos;
+using TimeTracker.Application.Validators;
 using TimeTracker.Core.BaseClasses;
+using TimeTracker.Core.Contracts;
 using AsyncOperation = System.Threading.Tasks.Task;
 
 namespace TimeTracker.Application.Commands
@@ -8,13 +14,49 @@ namespace TimeTracker.Application.Commands
     /// <summary>
     /// Update the selected task.
     /// </summary>
-    public class UpdateTaskCommand : DisposableBase, ICommand<Domain.Task>
+    public class UpdateTaskCommand : DisposableBase, ICommand<TaskDto>
     {
+        private readonly IRepositoryFactory _repositoryFactory;
+        private readonly Contracts.IValidatorFactory _validatorFactory;
+        private readonly IMapper _mapper;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="repositoryFactory">Handles data base operations.</param>
+        /// <param name="validatorFactory">Validate models.</param>
+        /// <param name="mapper">Map objects.</param>
+        public UpdateTaskCommand(IRepositoryFactory repositoryFactory,
+            Contracts.IValidatorFactory validatorFactory,
+            IMapper mapper)
+        {
+            _repositoryFactory = repositoryFactory;
+            _validatorFactory = validatorFactory;
+            _mapper = mapper;
+        }
+
         /// <inheritdoc/>
         /// <param name="task">Nullable parameter.</param>
-        public AsyncOperation Run(Domain.Task task)
+        /// /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ValidationException"/>
+        public AsyncOperation Run(TaskDto task)
         {
-            throw new NotImplementedException();
+            Guard.Against.Null(task, nameof(task));
+
+            var validator = _validatorFactory.GetInstance<TaskValidator>();
+
+            validator.ValidateAndThrow(task);
+
+            var repository = _repositoryFactory.GetRepository<Core.Task>();
+
+            using (var transaction = repository.GetTransaction())
+            {
+                var taskEntity = _mapper.Map<Core.Task>(task);
+
+                repository.Update(taskEntity);
+
+                return transaction.CommitAsync();
+            }
         }
     }
 }
---
+++ Tests/TimeTracker.Application.UnitTests/Commands/UpdateTaskCommandShould.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeTracker.Application.Commands;
+using TimeTracker.Application.Dtos;
+using TimeTracker.Application.Factories;
+using TimeTracker.Application.Queries;
+
+namespace TimeTracker.Application.UnitTests.Commands
+{
+    [TestClass]
+    public class UpdateTaskCommandShould
+    {
+        private UpdateTaskCommand _sut;
+        private CreateTaskCommand _createTaskCommand;
+        private GetTasksQuery _getTaskQuery;
+        private IMapper _mapper;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var commandFactory = AssemblyConfiguration.Kernel.Get<CommandFactory>();
+            var queryFactory = AssemblyConfiguration.Kernel.Get<QueryFactory>();
+
+            _mapper = AssemblyConfiguration.Kernel.Get<IMapper>();
+            _createTaskCommand = commandFactory.GetInstance<CreateTaskCommand>();
+            _getTaskQuery = queryFactory.GetInstance<GetTasksQuery>();
+            _sut = commandFactory.GetInstance<UpdateTaskCommand>();
+        }
+
+        [TestMethod]
+        public async Task GuardAgainstNull()
+        {
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(() => _sut.Run(null));
+        }
+
+        [TestMethod]
+        public void ValidateModelState()
+        {
+            var tempTask = new TaskDto()
+            {
+                Name = "",
+                GroupId = -1,
+                DescriptionId = -1
+            };
+
+            Func<Task> function = () => _sut.Run(tempTask);
+
+            using (new AssertionScope())
+            {
+                function.Should().Throw<ValidationException>()
+                    .And.Errors.Should()
+                    .Contain(x => x.PropertyName == nameof(TaskDto.Name));
+
+                function.Should().Throw<ValidationException>()
+                    .And.Errors.Should()
+                    .Contain(x => x.PropertyName == nameof(TaskDto.DescriptionId));
+
+                function.Should().Throw<ValidationException>()
+                    .And.Errors.Should()
+                    .Contain(x => x.PropertyName == nameof(TaskDto.GroupId));
+            }
+        }
+
+        [TestMethod]
+        public async Task UpdateATask()
+        {
+            var tempTask = new TaskDto() { Name = "Test task" };
+
+            await _createTaskCommand.Run(tempTask);
+
+            var result = await _getTaskQuery.Run(x => x.Name != null);
+
+            var task = result.FirstOrDefault();
+
+            task.Name = "Test task updated";
+
+            tempTask = _mapper.Map<TaskDto>(task);
+
+            await _sut.Run(tempTask);
+        }
+    }
+}
