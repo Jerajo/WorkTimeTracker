ee4a7c6fd09504a1a2a4db1096b0c0f099e213a7
Jesse <jessejose@outlook.es>
2020-04-04 09:23:39 -0400

    models updated

--- Source/TimeTracker.Core/ValueObjects/Description.cs
+++ Source/TimeTracker.Core/ValueObjects/Description.cs
@@ -6,7 +6,7 @@ namespace TimeTracker.Core.ValueObjects
     /// <summary>
     /// Represents the template to be use when the schedule is exported.
     /// </summary>
-    public partial class Description : EntityBase, IDescription
+    public class Description : EntityBase, IDescription
     {
         /// <inheritdoc/>
         public string Template { get; set; }
