ec0e7e7cb79654f7a5885d7cf4562d000bb0d565
Jesse <jessejose@outlook.es>
2020-04-05 20:56:04 -0400

    get current schedule query, and test completed

---
+++ Tests/TimeTracker.Application.UnitTests/Queries/GetCurrentScheduleQueryShould.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
+using System.Threading.Tasks;
+using TimeTracker.Application.Queries;
+
+namespace TimeTracker.Application.UnitTests.Queries
+{
+    [TestClass]
+    public class GetCurrentScheduleQueryShould
+    {
+        private GetCurrentScheduleQuery _sut;
+
+        [TestInitialize]
+        public void TextInitialize()
+        {
+            var kernel = AssemblyConfiguration.Kernel;
+            _sut = kernel.Get<GetCurrentScheduleQuery>();
+        }
+
+        [TestMethod]
+        public async Task GetCurrentSchedule()
+        {
+            var result = await _sut.Run();
+
+            result.Should().BeAssignableTo<Domain.Schedule>()
+                .And.NotBeNull();
+        }
+    }
+}
