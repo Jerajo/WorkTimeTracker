62bf38f676c06b1ed76a810da2b915757033ce34
Jesse <jessejose@outlook.es>
2020-04-06 00:53:28 -0400

    delete work time tracked command and test completed

---
+++ Source/TimeTracker.Application/Commands/DeleteWorkTimeTrackedCommand.cs
@@ -0,0 +1,62 @@
+﻿using Ardalis.GuardClauses;
+using AutoMapper;
+using FluentValidation;
+using System;
+using System.Threading.Tasks;
+using TimeTracker.Application.Contracts;
+using TimeTracker.Application.Dtos;
+using TimeTracker.Application.Validators;
+using TimeTracker.Core.BaseClasses;
+using TimeTracker.Core.Contracts;
+
+namespace TimeTracker.Application.Commands
+{
+    /// <summary>
+    /// Delete work time tracked for selected task.
+    /// </summary>
+    public class DeleteWorkTimeTrackedCommand : DisposableBase, ICommand<TasksScheduleDto>
+    {
+        private readonly IRepositoryFactory _repositoryFactory;
+        private readonly Contracts.IValidatorFactory _validatorFactory;
+        private readonly IMapper _mapper;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="repositoryFactory">Handles data base operations.</param>
+        /// <param name="validatorFactory">Validate models.</param>
+        /// <param name="mapper">Map objects.</param>
+        public DeleteWorkTimeTrackedCommand(IRepositoryFactory repositoryFactory,
+            Contracts.IValidatorFactory validatorFactory,
+            IMapper mapper)
+        {
+            _repositoryFactory = repositoryFactory;
+            _validatorFactory = validatorFactory;
+            _mapper = mapper;
+        }
+
+        /// <inheritdoc/>
+        /// <param name="taskSchedule">Nullable parameter.</param>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ValidationException"/>
+        public Task Run(TasksScheduleDto taskSchedule)
+        {
+            Guard.Against.Null(taskSchedule, nameof(taskSchedule));
+
+            var validator = _validatorFactory.GetInstance<TasksScheduleValidator>();
+
+            validator.ValidateAndThrow(taskSchedule, nameof(TasksScheduleDto.Id));
+
+            var repository = _repositoryFactory.GetRepository<Core.TasksSchedule>();
+
+            using (var transaction = repository.GetTransaction())
+            {
+                var taskEntity = _mapper.Map<Core.TasksSchedule>(taskSchedule);
+
+                repository.Delete(taskEntity);
+
+                return transaction.CommitAsync();
+            }
+        }
+    }
+}
---
+++ Tests/TimeTracker.Application.UnitTests/Commands/DeleteWorkTimeTrackedCommandShould.cs
@@ -0,0 +1,99 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeTracker.Application.Commands;
+using TimeTracker.Application.Dtos;
+using TimeTracker.Application.Factories;
+using TimeTracker.Application.Queries;
+
+namespace TimeTracker.Application.UnitTests.Commands
+{
+    [TestClass]
+    public class DeleteWorkTimeTrackedCommandShould
+    {
+        private GetCurrentScheduleQuery _getCurrentScheduleQuery;
+        private CreateScheduleCommand _createScheduleCommand;
+        private GetTasksScheduleQuery _getTasksScheduleQuery;
+        private TrackWorkTimeCommand _trackWorkTimeCommand;
+        private CreateTaskCommand _createTaskCommand;
+        private DeleteWorkTimeTrackedCommand _sut;
+        private GetTasksQuery _getTasksQuery;
+        private IMapper _mapper;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var commandFactory = AssemblyConfiguration.Kernel.Get<CommandFactory>();
+            var queryFactory = AssemblyConfiguration.Kernel.Get<QueryFactory>();
+            _mapper = AssemblyConfiguration.Kernel.Get<IMapper>();
+
+            _getCurrentScheduleQuery = queryFactory.GetInstance<GetCurrentScheduleQuery>();
+            _getTasksQuery = queryFactory.GetInstance<GetTasksQuery>();
+            _getTasksScheduleQuery = queryFactory.GetInstance<GetTasksScheduleQuery>();
+            _createTaskCommand = commandFactory.GetInstance<CreateTaskCommand>();
+            _createScheduleCommand = commandFactory.GetInstance<CreateScheduleCommand>();
+            _trackWorkTimeCommand = commandFactory.GetInstance<TrackWorkTimeCommand>();
+            _sut = commandFactory.GetInstance<DeleteWorkTimeTrackedCommand>();
+        }
+
+        [TestMethod]
+        public async Task GuardAgainstNull()
+        {
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(() => _sut.Run(null));
+        }
+
+        [TestMethod]
+        public void ValidateModelState()
+        {
+            var tempTaskSchedule = new TasksScheduleDto();
+
+            Func<Task> function = () => _sut.Run(tempTaskSchedule);
+
+            function.Should().Throw<ValidationException>()
+                .And.Errors.Should()
+                .Contain(x => x.PropertyName == nameof(TasksScheduleDto.Id));
+        }
+
+        [TestMethod]
+        public async Task DeleteWorkTimeTracked()
+        {
+            await _createTaskCommand.Run(new TaskDto
+            {
+                Name = nameof(DeleteWorkTimeTracked)
+            });
+
+            var tasks = await _getTasksQuery.Run(x => x.Name == nameof(DeleteWorkTimeTracked));
+
+            await _createScheduleCommand.Run(new ScheduleDto
+            {
+                ScheduleDate = DateTimeOffset.Now.Date
+            });
+
+            var task = tasks.First();
+            var schedule = await _getCurrentScheduleQuery.Run();
+
+            var tempTaskSchedule = new TasksScheduleDto()
+            {
+                Duration = TimeSpan.Zero,
+                TaskId = task.Id,
+                ScheduleId = schedule.Id,
+            };
+
+            await _trackWorkTimeCommand.Run(tempTaskSchedule);
+
+            var result = await _getTasksScheduleQuery.Run(x => x.TaskId == task.Id
+                                                            && x.ScheduleId == schedule.Id);
+
+            var trackedTime = result.First();
+
+            var timeToDelete = _mapper.Map<TasksScheduleDto>(trackedTime);
+
+            await _sut.Run(timeToDelete);
+        }
+    }
+}
