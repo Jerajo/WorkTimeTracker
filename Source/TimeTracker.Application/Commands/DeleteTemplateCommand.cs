a97d081037cd12442c87d6e6c3c2ea102424469d
Jesse <jessejose@outlook.es>
2020-04-06 00:52:09 -0400

    Delete template command and test completed

---
+++ Source/TimeTracker.Application/Commands/DeleteTemplateCommand.cs
@@ -0,0 +1,63 @@
+﻿using Ardalis.GuardClauses;
+using AutoMapper;
+using FluentValidation;
+using System;
+using System.Threading.Tasks;
+using TimeTracker.Application.Contracts;
+using TimeTracker.Application.Dtos;
+using TimeTracker.Application.Validators;
+using TimeTracker.Core.BaseClasses;
+using TimeTracker.Core.Contracts;
+using TimeTracker.Core.ValueObjects;
+
+namespace TimeTracker.Application.Commands
+{
+    /// <summary>
+    /// Delete the selected description.
+    /// </summary>
+    public class DeleteTemplateCommand : DisposableBase, ICommand<TaskExportTemplateDto>
+    {
+        private readonly IRepositoryFactory _repositoryFactory;
+        private readonly Contracts.IValidatorFactory _validatorFactory;
+        private readonly IMapper _mapper;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="repositoryFactory">Handles data base operations.</param>
+        /// <param name="validatorFactory">Validate models.</param>
+        /// <param name="mapper">Map objects.</param>
+        public DeleteTemplateCommand(IRepositoryFactory repositoryFactory,
+            Contracts.IValidatorFactory validatorFactory,
+            IMapper mapper)
+        {
+            _repositoryFactory = repositoryFactory;
+            _validatorFactory = validatorFactory;
+            _mapper = mapper;
+        }
+
+        /// <inheritdoc/>
+        /// <param name="template">Nullable parameter.</param>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ValidationException"/>
+        public Task Run(TaskExportTemplateDto template)
+        {
+            Guard.Against.Null(template, nameof(template));
+
+            var validator = _validatorFactory.GetInstance<TemplateValidator>();
+
+            validator.ValidateAndThrow(template, nameof(TaskExportTemplateDto.Id));
+
+            var repository = _repositoryFactory.GetRepository<Description>();
+
+            using (var transaction = repository.GetTransaction())
+            {
+                var templateEntity = _mapper.Map<Description>(template);
+
+                repository.Delete(templateEntity);
+
+                return transaction.CommitAsync();
+            }
+        }
+    }
+}
---
+++ Tests/TimeTracker.Application.UnitTests/Commands/DeleteTemplateCommandShould.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeTracker.Application.Commands;
+using TimeTracker.Application.Dtos;
+using TimeTracker.Application.Factories;
+using TimeTracker.Application.Queries;
+
+namespace TimeTracker.Application.UnitTests.Commands
+{
+    [TestClass]
+    public class DeleteTemplateCommandShould
+    {
+        private DeleteTemplateCommand _sut;
+        private CreateTemplateCommand _createTemplateCommand;
+        private GetTemplatesQuery _getTemplateQuery;
+        private IMapper _mapper;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var commandFactory = AssemblyConfiguration.Kernel.Get<CommandFactory>();
+            var queryFactory = AssemblyConfiguration.Kernel.Get<QueryFactory>();
+
+            _mapper = AssemblyConfiguration.Kernel.Get<IMapper>();
+            _createTemplateCommand = commandFactory.GetInstance<CreateTemplateCommand>();
+            _getTemplateQuery = queryFactory.GetInstance<GetTemplatesQuery>();
+            _sut = commandFactory.GetInstance<DeleteTemplateCommand>();
+        }
+
+        [TestMethod]
+        public async Task GuardAgainstNull()
+        {
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(() => _sut.Run(null));
+        }
+
+        [TestMethod]
+        public void ValidateModelState()
+        {
+            var templateTask = new TaskExportTemplateDto();
+
+            Func<Task> function = () => _sut.Run(templateTask);
+
+            function.Should().Throw<ValidationException>()
+                .And.Errors.Should()
+                .Contain(x => x.PropertyName == nameof(TaskExportTemplateDto.Id));
+        }
+
+        [TestMethod]
+        public async Task DeleteATemplate()
+        {
+            var tempTemplate = new TaskExportTemplateDto() { Template = nameof(DeleteATemplate) };
+
+            await _createTemplateCommand.Run(tempTemplate);
+
+            var result = await _getTemplateQuery.Run(x => x.Template == tempTemplate.Template);
+
+            var task = result.First();
+
+            tempTemplate = _mapper.Map<TaskExportTemplateDto>(task);
+
+            await _sut.Run(tempTemplate);
+        }
+    }
+}
