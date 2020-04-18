d6e7576d206f6dbe4f2f7f31a414418e3b9af3f4
Jesse <jessejose@outlook.es>
2020-04-18 15:04:05 -0400

    Booststrapper moved to configuration

---
+++ Source/TimeTracker.Xamarin/Configuration/Booststrapper.cs
@@ -0,0 +1,31 @@
+﻿using Ninject;
+using Prism.Ioc;
+using TimeTracker.Application.Contracts;
+using TimeTracker.Application.Factories;
+using TimeTracker.Xamarin.Shell;
+
+namespace TimeTracker.Xamarin.Configuration
+{
+    public static class Booststrapper
+    {
+        public static IContainerRegistry RegisterNavigation(this IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<MainShell, MainShellModel>();
+            return containerRegistry;
+        }
+
+        public static IContainerRegistry RegisterTypes(this IContainerRegistry containerRegistry)
+        {
+            containerRegistry.Register<Contracts.ICommandFactory, Services.CommandFactory>();
+            containerRegistry.Register<IQueryFactory, QueryFactory>();
+            return containerRegistry;
+        }
+
+        public static IContainerRegistry RegisterContainer(this IContainerRegistry containerRegistry,
+            IKernel kernel)
+        {
+            containerRegistry.RegisterInstance<IKernel>(kernel);
+            return containerRegistry;
+        }
+    }
+}
