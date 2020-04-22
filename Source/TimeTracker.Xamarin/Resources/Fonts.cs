8b2baa0de14ef76b446ef01d3c680aaa28f7e6f8
Jesse <jessejose@outlook.es>
2020-04-22 18:32:31 -0400

    Resources encapsulated

---
+++ Source/TimeTracker.Xamarin/Resources/Fonts.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace TimeTracker.Xamarin.Resources
+{
+    public static class Fonts
+    {
+        public static string MaterialRegular =>
+            (OnPlatform<string>)App.Current.Resources["FontAwesomeRegularIcons"];
+        public static string MaterialSolid =>
+            (OnPlatform<string>)App.Current.Resources["FontAwesomeSolidIcons"];
+        public static string MaterialBrand =>
+            (OnPlatform<string>)App.Current.Resources["FontAwesomeBrandIcons"];
+    }
+}
