21ce04b7ba78fd93dc8049538512df141202054b
Jesse <jessejose@outlook.es>
2020-04-22 18:33:20 -0400

    Services for container and region navigation created

---
+++ Source/TimeTracker.Xamarin/Services/ContainerExtensions.cs
@@ -0,0 +1,42 @@
+﻿using DryIoc;
+using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace TimeTracker.Xamarin.Services
+{
+    public static class ContainerExtensions
+    {
+        private static readonly Dictionary<string, (Type, Type)> _navigationContainer
+            = new Dictionary<string, (Type, Type)>();
+
+        public static IContainerRegistry RegisterRegion<TView, TViewModel>(
+            this IContainerRegistry containerRegistry,
+            string name = null)
+            where TView : BindableObject
+            where TViewModel : class
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                name = typeof(TView).Name;
+
+            _navigationContainer.Add(name, (typeof(TView), typeof(TViewModel)));
+            containerRegistry.Register<TViewModel>();
+            containerRegistry.Register<TView>();
+
+            return containerRegistry;
+        }
+
+        public static View ResolveRegion(this IContainer container,
+            string name)
+        {
+            var (viewType, viewModelType) = _navigationContainer[name];
+
+            var view = (View)container.Resolve(viewType, IfUnresolved.Throw);
+            var viewModel = container.Resolve(viewModelType, IfUnresolved.Throw);
+
+            view.BindingContext = viewModel;
+            return view;
+        }
+    }
+}
