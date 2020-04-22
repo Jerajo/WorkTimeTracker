230c4c07c5560fdeb46f1e823f5b7ba98b73c170
Jesse <jessejose@outlook.es>
2020-04-22 18:30:12 -0400

    Domain modules created

---
+++ Source/TimeTracker.Xamarin/Domains/Task/TasksRegion.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TimeTracker.Xamarin.Domains.Task
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TasksRegion : ContentView
+    {
+        public TasksRegion()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Source/TimeTracker.Xamarin/Domains/Task/TasksRegionModel.cs
@@ -0,0 +1,16 @@
+﻿using TimeTracker.Application.Contracts;
+using TimeTracker.Xamarin.Contracts;
+using ICommandFactory = TimeTracker.Xamarin.Contracts.ICommandFactory;
+
+namespace TimeTracker.Xamarin.Domains.Task
+{
+    public class TasksRegionModel : RegionModelBase
+    {
+        public TasksRegionModel(IRegionNavigationService navigationService,
+            ICommandFactory commandFactory,
+            IQueryFactory queryFactory)
+            : base(navigationService, commandFactory, queryFactory)
+        {
+        }
+    }
+}
