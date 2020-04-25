998649a4020c50cf645fc08ebad121cd263da288
Jesse <jessejose@outlook.es>
2020-04-25 18:36:26 -0400

    UI updated

--- Source/TimeTracker.Xamarin/Layout/NavigationMenu/DeskTopNavigationMenuModel.cs
+++ Source/TimeTracker.Xamarin/Layout/NavigationMenu/DeskTopNavigationMenuModel.cs
@@ -1,52 +1,38 @@
-﻿using Prism.Services;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using TimeTracker.Core.Resources;
 using TimeTracker.Xamarin.Contracts;
 using TimeTracker.Xamarin.Resources;
 using TimeTracker.Xamarin.Services;
 using Xamarin.Forms.PowerControls.Buttons;
 using Xamarin.Forms.PowerControls.Menus;
-using Xamarin.Forms.PowerControls.Resources.Fonts;
 
 namespace TimeTracker.Xamarin.Layout.NavigationMenu
 {
     public class DeskTopNavigationMenuModel : NavigationMenuModel
     {
         private readonly IRegionNavigationService _navigationService;
-        private readonly IPageDialogService _pageDialogService;
+        private readonly IDialogService _dialogService;
 
         public DeskTopNavigationMenuModel(IRegionNavigationService navigationService,
-            IPageDialogService pageDialogService)
+            IDialogService dialogService)
         {
             _navigationService = navigationService;
-            _pageDialogService = pageDialogService;
-
-            //TODO: implement load menu from persistence.
-            Tabs = new List<NavigationButtonModel>
-            {
-                new NavigationButtonModel(Regions.Tasks,
-                    Regions.Tasks,
-                    Fonts.MaterialRegular,
-                    FontAwesomeRegular.CheckSquare),
-                new NavigationButtonModel(Regions.Schedules,
-                    Regions.Schedules,
-                    Fonts.MaterialRegular,
-                    FontAwesomeRegular.Clock),
-                new NavigationButtonModel(Regions.Options,
-                    Regions.Options,
-                    Fonts.MaterialSolid,
-                    FontAwesomeSolid.Cog),
-            };
+            _dialogService = dialogService;
+            Tabs = UiComponents.GetNavigationButtonModelss();
         }
 
         public List<NavigationButtonModel> Tabs { get; }
 
         #region Auxiliary Methods
 
-        protected override void OnTabSelected(object selectedTab)
+        protected override async void OnTabSelected(object selectedTab)
         {
             if (!(selectedTab is NavigationButtonModel model))
+            {
+                await _dialogService.DisplayErroMessage(Messages.DialogNavigationErrorMessage);
                 return;
+            }
             NavigateTo(model.Path);
         }
 
@@ -59,20 +45,19 @@ namespace TimeTracker.Xamarin.Layout.NavigationMenu
             {
                 var navigateBackResult =  await _navigationService.NavigateBackAsync();
                 if (!navigateBackResult.Success)
-                    await ShowNavigationErroMessage(navigateBackResult.Exception.Message);
+                {
+                    await _dialogService.DisplayErroMessage(Messages.DialogNavigationErrorMessage);
+                    Debug.WriteLine(navigateBackResult.Exception.Message);
+                }
                 return;
             }
 
             var navigateToResult = await _navigationService.NavigateToAsync(path);
             if (!navigateToResult.Success)
-                await ShowNavigationErroMessage(navigateToResult.Exception.Message);
-        }
-
-        private async Task ShowNavigationErroMessage(string exception)
-        {
-            await _pageDialogService.DisplayAlertAsync("Error",
-                "No se pudo navegar a la region seleccionada. \n" + exception,
-                "Aceptar");
+            {
+                await _dialogService.DisplayErroMessage(Messages.DialogNavigationErrorMessage);
+                Debug.WriteLine(navigateToResult.Exception.Message);
+            }
         }
 
         #endregion
