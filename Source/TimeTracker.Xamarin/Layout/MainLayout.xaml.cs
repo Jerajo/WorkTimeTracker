38258ed9c13bf0632c1419a013e3a35b5e44c7ca
Jesse <jessejose@outlook.es>
2020-05-01 17:24:47 -0400

    notification region added to the main layout

--- Source/TimeTracker.Xamarin/Layout/MainLayout.xaml.cs
+++ Source/TimeTracker.Xamarin/Layout/MainLayout.xaml.cs
@@ -1,13 +1,16 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using TimeTracker.Xamarin.Layout.Notifications;
+using Xamarin.Forms.Xaml;
 
 namespace TimeTracker.Xamarin.Layout
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainLayout
     {
-        public MainLayout()
+        public MainLayout() => InitializeComponent();
+
+        public async void PushNotification(Notification notification, int? millisecondsDuration = null)
         {
-            InitializeComponent();
+            await Notifications.Push(notification, millisecondsDuration);
         }
     }
 }
