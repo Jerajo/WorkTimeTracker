69b0a5ea72bb26866b4519a90e315e7045425094
Jesse <jessejose@outlook.es>
2020-05-01 17:18:03 -0400

    live editor for groups added

---
+++ Source/TimeTracker.Xamarin/Layout/Shared/LiveEditor.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms.Xaml;
+using FocusEventArgs = Xamarin.Forms.FocusEventArgs;
+
+namespace TimeTracker.Xamarin.Layout.Shared
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LiveEditor
+    {
+        public LiveEditor() : this(null) {}
+        public LiveEditor(object bindingContext)
+        {
+            InitializeComponent();
+            BindingContext = bindingContext;
+        }
+
+        public event EventHandler StopEditing;
+
+        private async void Entry_Unfocused(object sender, FocusEventArgs e)
+        {
+            await System.Threading.Tasks.Task.Delay(100);
+            if (!Children.Any(x => x.IsFocused))
+                StopEditing?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void FocusEntry()
+        {
+            CodeEntry.Focus();
+        }
+    }
+}
