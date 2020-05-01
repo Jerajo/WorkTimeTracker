2815798c07ce833eca13fc31fc20e6ba4b80dfb6
Jesse <jessejose@outlook.es>
2020-05-01 17:14:56 -0400

    some tests added to shedules region

--- Source/TimeTracker.Xamarin/Domains/Schedule/SchedulesRegion.xaml.cs
+++ Source/TimeTracker.Xamarin/Domains/Schedule/SchedulesRegion.xaml.cs
@@ -1,14 +1,20 @@
-﻿using Xamarin.Forms;
+﻿using Syncfusion.XForms.Buttons;
 using Xamarin.Forms.Xaml;
 
 namespace TimeTracker.Xamarin.Domains.Schedule
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class SchedulesRegion : ContentView
+    public partial class SchedulesRegion
     {
         public SchedulesRegion()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            if (!(sender is SfButton view))
+                return;
         }
     }
 }
