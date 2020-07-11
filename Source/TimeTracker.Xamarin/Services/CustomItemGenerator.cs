ef9ca5dc3c71c4180af3881d693bcc674e8d4dfd
Jesse <jessejose@outlook.es>
2020-07-11 15:51:48 -0400

    custom item services

---
+++ Source/TimeTracker.Xamarin/Services/CustomItemGenerator.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.ListView.XForms;
+using System.Windows.Input;
+
+namespace TimeTracker.Xamarin.Services
+{
+    public class CustomItemGenerator : ItemGenerator
+    {
+        private SfListView _listView;
+        private ICommand _editCommand;
+        private ICommand _deleteCommand;
+
+        public CustomItemGenerator(SfListView listView,
+            ICommand editCommand,
+            ICommand deleteCommand)
+            : base(listView)
+        {
+            _listView = listView;
+            _editCommand = editCommand;
+            _deleteCommand = deleteCommand;
+        }
+
+        protected override ListViewItem OnCreateListViewItem(int itemIndex, ItemType type, object data = null)
+        {
+            if (type == ItemType.Record)
+                return new CustomListViewItem(_editCommand, _deleteCommand);
+
+            return base.OnCreateListViewItem(itemIndex, type, data);
+        }
+    }
+}
